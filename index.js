40bda691da892982a27320d74a8bf30f20cf3764
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-05-26 16:37:54 +0200

    Puntos 1 y 2 del ejercicio 02

---
+++ index.js
@@ -0,0 +1,11 @@
+// Punto 1
+
+const frase = "amariolegustanlosgatos";
+
+console.log(frase[0] + frase[1].toUpperCase() + frase.slice(2, 6) + frase[6].toUpperCase() + frase[7] + frase[8].toUpperCase() + frase.slice(9, 14) + frase[14].toUpperCase() + frase.slice(15, 17) + frase[17].toUpperCase() + frase.slice(18));
+
+// Punto 2
+
+const string = "fraseMasCorta";
+
+console.log(string.slice(0, 5) + string[5].toLowerCase() + string.slice(6, 8) + string[8].toLowerCase() + string.slice(9));

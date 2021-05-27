3ab6044113117519014bee1f1939adca4af77d67
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-05-27 10:39:27 +0200

    Corrección ejercicio

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 // Punto 1
 
-const frase = "amariolegustanlosgatos";
+/* const fraseSnakeCase = "a_mario_le_gustan_los_gatos";
 
-console.log(frase[0] + frase[1].toUpperCase() + frase.slice(2, 6) + frase[6].toUpperCase() + frase[7] + frase[8].toUpperCase() + frase.slice(9, 14) + frase[14].toUpperCase() + frase.slice(15, 17) + frase[17].toUpperCase() + frase.slice(18));
+const palabrasSeparadas = fraseSnakeCase.split("_");
+
+let palabrasModificadas = palabrasSeparadas[0];
+
+for (const palabra of palabrasSeparadas.slice(1)) {
+    const inicialMayuscula = palabra.charAt(0).toUpperCase() + palabra.slice(1);
+    palabrasModificadas += inicialMayuscula;
+}
+
+console.log(palabrasModificadas); */
 
 // Punto 2
 
-const string = "fraseMasCorta";
+const fraseCamelCase = "aMarioNoLeGustanLosFloats";
+let fraseSnakeCase = "";
 
-console.log(string.slice(0, 5) + string[5].toLowerCase() + string.slice(6, 8) + string[8].toLowerCase() + string.slice(9));
+for (const caracter of fraseCamelCase) {
+    if (caracter.toUpperCase() === caracter) {
+        fraseSnakeCase += `_${  caracter.toLowerCase()}`
+    } else {
+        fraseSnakeCase += caracter;
+    }
+}
+
+console.log(fraseSnakeCase);
